0d24133b6ed051cdbea4321d46448c17b6a1454c
minsun <elinnane0@gmail.com>
2023-11-15 15:46:13 +0900

    ✨ SWEA 평균과 평탄화

--- SWEA/1208.Flatten.py
+++ SWEA/1208.Flatten.py
@@ -11,7 +11,6 @@ def dumps(numberList):
     return maxIndex, minIndex
     
 T = 10
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     n = int(input())
     numberList = list(map(int, input().split()))
