611c40fdf128d652ce552aa9f5c433b68e39317f
minsun <elinnane0@gmail.com>
2023-11-20 14:28:38 +0900

    ✨ part1 sort, groupby

---
+++ part1/T14-상위값.py
@@ -0,0 +1,13 @@
+# %% [code]
+# city와 f4를 기준으로 f5의 평균값을 구한 다음, 
+# f5를 기준으로 상위 7개 값을 모두 더해 출력하시오 (소수점 둘째자리까지 출력)
+# - 데이터셋 : basic1.csv 
+
+import pandas as pd
+df = pd.read_csv("../input/bigdatacertificationkr/basic1.csv")
+df = df.groupby(['city', 'f4'])[['f5']].mean() ## WHY???? []????
+
+df = df.reset_index().sort_values('f5', ascending=False).head(7) # dataframe 전환 후 상위 7개 출력
+print(round(df['f5'].sum(), 2))
+
+# #결과값 : 643.68
