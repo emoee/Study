3ce77d061176c81deadec7b600457f3aedc25a24
minsun <elinnane0@gmail.com>
2023-12-01 20:29:25 +0900

    ♻️ part 1 - review[:20]

--- part1/T17-시계열.py
+++ part1/T17-시계열.py
@@ -1,4 +1,19 @@
 # 2022년 5월 sales의 중앙값을 구하시오
+
+# 데이터셋 : basic2.csv
+# 오른쪽 상단 copy&edit 클릭 -> 예상문제 풀이 시작
+import pandas as pd
+df = pd.read_csv("../input/bigdatacertificationkr/basic2.csv")
+
+df['Date'] = pd.to_datetime(df['Date'])
+df['year'] = df['Date'].dt.year
+df['month'] = df['Date'].dt.month
+con = (df['year'] == 2022) & (df['month'] == 5)
+
+result = df[con]['Sales'].median()
+print(result)
+
+# 2022년 5월 sales의 중앙값을 구하시오
 # 데이터셋 : basic2.csv
 
 import pandas as pd
--- part1/T18-시계열2.py
+++ part1/T18-시계열2.py
@@ -1,4 +1,26 @@
 # 주어진 데이터에서 2022년 5월 주말과 평일의 sales컬럼 평균값 차이를 구하시오 (소수점 둘째자리까지 출력, 반올림)
+
+# 데이터셋 : basic2.csv
+# 오른쪽 상단 copy&edit 클릭 -> 예상문제 풀이 시작
+
+import pandas as pd
+df = pd.read_csv("../input/bigdatacertificationkr/basic2.csv")
+df['Date'] = pd.to_datetime(df['Date'])
+df['year'] = df['Date'].dt.year
+df['month'] = df['Date'].dt.month
+df['wo'] = df['Date'].dt.dayofweek
+
+con = (df['year'] == 2022)  & (df['month'] == 5)
+df = df[con]
+con1 = (df['wo'] >= 5)
+con2 = (df['wo'] < 5)
+weekend = df[con1]['Sales'].mean()
+weekday = df[con2]['Sales'].mean()
+result = round(abs(weekend - weekday),2)
+print(result)
+
+
+# 주어진 데이터에서 2022년 5월 주말과 평일의 sales컬럼 평균값 차이를 구하시오 (소수점 둘째자리까지 출력, 반올림)
 # 데이터셋 : basic2.csv
 
 import pandas as pd
